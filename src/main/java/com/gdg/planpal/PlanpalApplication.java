72d19e6d856d41977dc9693518a9801241a156e1
hyunwoo song <yunuo468@gmail.com>
2025-04-07 20:18:24 +0900

    project init

---
+++ src/main/java/com/gdg/planpal/PlanpalApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.planpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanpalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdg/planpal/PlanpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.planpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
