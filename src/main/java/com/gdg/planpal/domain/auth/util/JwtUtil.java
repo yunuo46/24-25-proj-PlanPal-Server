84f87d209b6a37b39192fe48699c02d6d7197317
Hyunwoo Song <yunuo468@gmail.com>
2025-04-16 18:20:48 +0900

    fix: cors error

--- src/main/java/com/gdg/planpal/domain/auth/util/JwtUtil.java
+++ src/main/java/com/gdg/planpal/domain/auth/util/JwtUtil.java
@@ -4,17 +4,23 @@ import com.gdg.planpal.domain.auth.dto.Tokens;
 import com.gdg.planpal.domain.auth.dto.response.TokenResponse;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
 
 @Component
 public class JwtUtil {
 
     public static void setRefreshTokenInCookie(HttpServletResponse response, String refreshToken) {
-        Cookie cookie = new Cookie("refreshToken", refreshToken);
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(5 * 24 * 60 * 60);
-        response.addCookie(cookie);
+        ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .path("/")
+                .maxAge(Duration.ofDays(5))
+                .build();
+        response.setHeader("Set-Cookie", cookie.toString());
     }
 
     public static TokenResponse setJwtResponse(HttpServletResponse response, Tokens tokenDto) {
