c155e089ec6b4fcf4444514bafd996fdc49d2782
Hyunwoo Song <yunuo468@gmail.com>
2025-05-02 15:56:58 +0900

    feat: map pin fetch join query

--- src/main/java/com/gdg/planpal/domain/map/dao/MapRepository.java
+++ src/main/java/com/gdg/planpal/domain/map/dao/MapRepository.java
@@ -8,6 +8,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface MapRepository extends JpaRepository<MapBoard, Long> {
-    @Query("SELECT m FROM MapBoard m LEFT JOIN FETCH m.pins WHERE m.chatRoom.id = :chatRoomId")
+    @Query("""
+       SELECT m FROM MapBoard m
+       JOIN FETCH m.pins
+       WHERE m.chatRoom.id = :chatRoomId
+    """)
     Optional<MapBoard> findWithPinsByChatRoomId(@Param("chatRoomId") Long chatRoomId);
 }
